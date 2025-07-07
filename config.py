12d20fbe41f43df44fb18a372fddc2ec8986ec2f
GOMZEE <130956429+krazystar55@users.noreply.github.com>
2025-07-07 17:21:47 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TELEGRAM_BOT_API_KEY = os.getenv("7835794353:AAEvGDPagNHEbc1rk59_92OuyNLUjwLgDUI")
+LEAKCHECK_API_KEY = os.getenv("639f9dff96f6f9ccde7f97b140c721e94793dec4")
+LEAK_DIR = os.getenv("LEAK_DIR")
